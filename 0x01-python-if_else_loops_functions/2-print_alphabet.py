724897bfb7c3c43377fb0673f93c450e076bbb7e
S664470 <shahialtayeb@gmail.com>
2023-03-10 07:52:04 -0800

    commit

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
-for i in rang(a , z)
-print(i)
+#!/usr/bin/python3
+for i in range(97, 122):
+    print("{:c}". format(i), end=" ")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/user/bin/python3
+for i in range(0, 98):
+    print("{:d} = 0x{:0x}". format(i, i))
