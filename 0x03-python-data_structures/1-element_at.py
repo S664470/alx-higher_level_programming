c2b750de9fe292228b5bf09307951072e2f4207a
S664470 <shahialtayeb@gmail.com>
2023-03-12 09:49:45 -0700

    commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,7 @@
 def element_at(my_list, idx):
     for i in my_list:
         if idx < 0:
-            return(None)
-        elif idx >= len(my_lis):
-            return(None)
-        for i in range(len(my_list)):
-            if i == idx:
-                return(my_list[i])
-        
+            return (None)
+        elif idx > len(my_list):
+            return (None)
+        return (my_list[idx])
