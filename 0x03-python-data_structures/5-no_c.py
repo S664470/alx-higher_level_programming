8fa759f5678dc0926bc761d1ccb0297785ff9b43
S664470 <shahialtayeb@gmail.com>
2023-03-12 19:26:32 +0000

    commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-        for ch in range(my_string):
-            if ch != 'c' and ch != 'C':
-                return(ch)
+    noC = " "
+    for ch in my_string:
+        if ch != 'c' and ch != 'C':
+            noC += ch
+    return (noC)
