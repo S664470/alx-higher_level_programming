e48b5dff40e110ec8c5aab674ddfb51bec63af50
S664470 <shahialtayeb@gmail.com>
2023-04-03 11:54:54 -0700

    commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
+# 1-rectangle.py
 # Auth: Shahinda Altayeb
+
+"""class name Rectangle"""
+
+
 class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
 
-    def width(self):
-        return self.__width
-    def width(self, value):
-        if not type(value):
-             raise TypeError("width must be an integer")
-        if value < 0:
-             raise ValueError("width must be >= 0")
-             self.__width = value
+        """Retarve height"""
+        def width(self):
+            return self.__height
 
-    def height(self):
-        return self.__height
-    def height(self, value):
-        if not type(value):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-            self.__height = value
+        """Git the value of the height"""
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            elif value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+
+        """Retarve width"""
+        def height(self):
+            return self.__width
+
+        """Git width value"""
+        def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
