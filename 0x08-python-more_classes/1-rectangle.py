a894ab9004277a6b35a75c94b93aebf71d02472d
S664470 <shahialtayeb@gmail.com>
2023-04-05 13:23:43 -0700

    commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-# 1-rectangle.py
-# Auth: Shahinda Altayeb
-
-"""class name Rectangle"""
-
-
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
-    property
+    @property
     def width(self):
         return self.__width
 
+    @width.setter
     def width(self, value):
-        if not type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-             self.__width = value
+        self.__width = value
 
-    property
+    @property
     def height(self):
         return self.__height
 
+    @height.setter
     def height(self, value):
-        if not type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
