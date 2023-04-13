69fc79e7a6e946a51ffda713da702fd779ad811b
S664470 <shahialtayeb@gmail.com>
2023-04-13 16:15:10 -0700

    commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Creat AN Obiect from “JSON file”
+"""
+
+
+import json
+
+def load_from_json_file(filename):
+    """
+    open the file for reding
+    ---
+    load the jason data
+    """
+
+    with open(filename,'r') as f:
+        obj = json.load(f)
+        return obj
