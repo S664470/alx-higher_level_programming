e30c0e874d8627b71a35dcc51ae2538663bcdec5
S664470 <shahialtayeb@gmail.com>
2023-06-06 23:08:28 -0700

    commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python3
 """
-This is a python function to add 2 interger
+This is the  "0-add_integer" module.
+The 0-add_integer module supplies one function, add_integer(a, b).
 """
+
+
 def add_integer(a, b=98):
-    """
-    python qdd two interger a and b in which b have a constant value
-    """
-    if (not isinstance(a, int) and not isinstance(b, flot)):
-        raise TypeError("a must be an integer")
-    if (not isinstance(b, int) and not isinstance(b, flot)):
-        raise TypeError("b must be an flot")
-    a = int(a)
-    b = int(b)
+    """Return the addition of two integers"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
+    a = int(a) if isinstance(a, float) else a
+    b = int(b) if isinstance(b, float) else b
     return a + b
