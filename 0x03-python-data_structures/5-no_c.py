903a7d112167730df4d6623ae3fa829d65fe5514
S664470 <113641235+S664470@users.noreply.github.com>
2023-07-05 08:47:47 +0200

    Commit

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-def no_c(my_string):
-    noC = " "
-    for ch in my_string:
-        if ch != 'c' and ch != 'C':
-            noC += ch
-    return (noC)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-
-    for i in matrix:
-        for j in i:
-            print("{:d}". format(j))
-            if j != i[-1]:
-                print(end=" ")
-                print()
