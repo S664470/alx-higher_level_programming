7d198fa400d8dcd01e336cc729ef761a0e7e74cb
S664470 <113641235+S664470@users.noreply.github.com>
2023-07-05 08:49:06 +0200

    Commit

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def safe_print_integer_err(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
-    print("Exception: Unknown format code {} for object of type {}")
--- 0x05-python-exceptions/README.md
+++
@@ -1,1 +0,0 @@
-Even if a statement or expression is syntactically correct
