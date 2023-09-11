73696e48c65d0f91076a191fccd743c682d9741a
S664470 <shahialtayeb@gmail.com>
2023-09-11 03:14:28 -0700

    commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,9 @@ contain list of class
 
 """
 
+
 class MyList(list):
     """class that inherat from another"""
-
     def print_sorted(self):
         """print sorted list"""
         print(sorted(self))
