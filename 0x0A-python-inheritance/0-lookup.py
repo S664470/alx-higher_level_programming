f873048903920b2b59f0b57832ffffbafbd9912b
S664470 <shahialtayeb@gmail.com>
2023-09-11 03:11:43 -0700

    commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,5 +7,4 @@ lookup function
 
 def lookup(obj):
     """returns a list of available attributes and methods of an object"""
-
     return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contain list of class
+
+"""
+
+class MyList(list):
+    """class that inherat from another"""
+
+    def print_sorted(self):
+        """print sorted list"""
+        print(sorted(self))
