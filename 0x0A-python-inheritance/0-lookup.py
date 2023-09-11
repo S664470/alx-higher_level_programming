833aede15a77d3e93780a010c7993738b6e833d7
S664470 <shahialtayeb@gmail.com>
2023-09-11 02:27:24 -0700

    commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that return list of method and attribute of an objecg used"""
+
+
+def lookup(obj):
+    """dir() is used to return list of a method and object used"""
+
+    return dir(list)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+in python, inheritance is a fundamental concept of object-oriented programmingn that allows one class to inherate attribute and methods from another.
