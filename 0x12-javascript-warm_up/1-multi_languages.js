66faa8a3e59ffeb97be1fb5e5016039f0927617b
S664470 <shahialtayeb@gmail.com>
2023-11-13 02:15:11 -1200

    commit

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 //Script that print 3 lines of code
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log("myVar");
