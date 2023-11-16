d5d9e93fe87e1f997f7a4930a0e25be35ace63ac
S44770 <shahialtayeb@gmail.com>
2023-11-15 16:49:51 -0800

    commit

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -8,9 +8,8 @@ module.exports = class Rectangle {
     for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 
- 
   rotate () {
-    [this.width, this.height] = [this.height, this.width]
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
