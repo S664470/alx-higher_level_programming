3cf0fcc8da19d60ee3fe42d759cb4908b3d7048b
S44770 <shahialtayeb@gmail.com>
2023-12-03 08:41:24 -0800

    commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Feacth hbtn URL"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode("utf-8")))
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
