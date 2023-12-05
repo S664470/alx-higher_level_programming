fccb81af40b979d524d5798c5ba08666fcd9c666
S44770 <shahialtayeb@gmail.com>
2023-12-05 06:59:17 -0800

    commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,8 +10,8 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    email = sys.argv[2]
+    data = urllib.parse.urlencode(email).encode("ascii")
 
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
